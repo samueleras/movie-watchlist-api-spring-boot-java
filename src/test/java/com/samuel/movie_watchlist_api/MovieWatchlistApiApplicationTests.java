53ede5110387f9211bbf5d87519736faa35ed472
Samuel Eras <sa.eras@gmx.de>
2025-11-05 15:24:07 +0100

    feat: set up spring boot project with java 21 and maven

---
+++ src/test/java/com/samuel/movie_watchlist_api/MovieWatchlistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuel.movie_watchlist_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieWatchlistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
