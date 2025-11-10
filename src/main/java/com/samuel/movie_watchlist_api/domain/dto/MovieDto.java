7537f323c47795ce491158fff13f83473c7b9bfa
Samuel Eras <sa.eras@gmx.de>
2025-11-10 11:14:17 +0100

    feat: add Relation annotation to set relation names
    - Ensures _embedded.movies appears instead of default movieDtoList
    - Makes HATEOAS JSON more readable and consistent

--- src/main/java/com/samuel/movie_watchlist_api/domain/dto/MovieDto.java
+++ src/main/java/com/samuel/movie_watchlist_api/domain/dto/MovieDto.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.hateoas.server.core.Relation;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Relation(itemRelation = "movie", collectionRelation = "movies")
 public class MovieDto {
 
     private Long id;
